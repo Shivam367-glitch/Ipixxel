5df7879c0a8863c288d05df6b0c04dca08b79a39
Shivam <txt2shivam@gmail.com>
2024-08-09 18:57:02 +0530

    About  page work

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -1,13 +1,22 @@
+import { Col, Container, Row } from "react-bootstrap"
 import Contact from "../../components/Contact/Contact"
-
+import HeroSection  from "../../components/Hero/HeroSection"
 
 const About = () => {
   return (
-    <div>
+    <Container fluid={true} className="m-0 p-0">
+      <Row className="d-flex flex-column m-0  p-0">
 
-
+      <Col className="col-12 m-0 p-0">
+      <HeroSection background="aboutHero.webp" heading="About Us"/>
+      </Col>
+      <Col className="col-12 mt-4">
       <Contact background="Contact.webp"/>
-    </div>
+
+      </Col>
+      </Row>
+
+    </Container>
   )
 }
 
