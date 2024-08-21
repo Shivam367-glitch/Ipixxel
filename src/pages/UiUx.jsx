f03b9abf33478f1380e539215caa662174d38854
Shivam <txt2shivam@gmail.com>
2024-08-21 18:36:02 +0530

    Added Order Section in Digital Marketing page

--- src/pages/UiUx.jsx
+++ src/pages/UiUx.jsx
@@ -1,8 +1,8 @@
 import { Col, Container, Row } from "react-bootstrap"
 import Contact from "../components/Contact/Contact"
 import HeroSection from "../components/Hero/HeroSection"
-
- 
+import OrderSection from "../components/Order/OrderSection"
+import {serviceList} from "../Utility/service.json"
 
 const UiUx = () => {
   return (
@@ -10,8 +10,11 @@ const UiUx = () => {
     <Row> 
     <Col className="col-12 m-0 p-0">
           <HeroSection background="uiHero.jpg" heading="UX / UI" subHeading="Design" backgroundPosition="top left"/>
-        </Col>
-    <Col className="col-12 mt-4 p-md-4  wow animate__animated animate__zoomIn">
+    </Col> 
+    <Col className="m-0 p-0 col-12 mt-4 p-md-4">
+      <OrderSection heading={"Different UX/UI design services we offer"} card={serviceList[2]["specificService"]}/>
+     </Col>
+      <Col className="col-12 mt-4 p-md-4  wow animate__animated animate__zoomIn">
           <Contact background="Contact.webp" />
         </Col>
     </Row>
