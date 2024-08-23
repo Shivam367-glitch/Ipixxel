46502a630ca9a9e3ca061ccd11fa06a06518041c
Shivam <txt2shivam@gmail.com>
2024-08-23 12:10:09 +0530

    Added Description Section in Digital Marketing Page

--- src/components/DescriptionSection.jsx
+++ src/components/DescriptionSection.jsx
@@ -10,17 +10,17 @@ const DescriptionSection = (props) => {
         <p className="display-6 fw-light">{heading}</p>
         
         </Col>
-        <Col className="container-md  col-12  py-5  "> 
+        <Col className="container-md  col-12"> 
           <Container fluid={true}>
-          <Row  className="justify-content-around ">
-            <Col className="col-12 col-md-5 align-self-md-center m-0 p-0">
+          <Row  className="justify-content-around">
+            <Col className="col-12 col-md-6 col-lg-5  align-self-md-center m-0 p-0">
               {
                 paragraphs.map((para, ind) => (
                   <p className="fw-medium mt-2 fs-5 text-justify" key={ind}>{para}</p>
               ))
               }
             </Col>
-            <Col className="col-12 col-md-6 mt-2 m-md-0 p-0">
+            <Col className="col-12 col-md-5  col-lg-6  mt-2 m-md-0 p-0 text-center">
                 <img decoding="async"  src={imgInfo.src} className="img-fluid wow animate__animated animate__backInRight" alt="" 
                 srcSet={imgInfo.srcSet} sizes={imgInfo.sizes}></img>
             </Col>
--- src/pages/DigitalMarketing.jsx
+++ src/pages/DigitalMarketing.jsx
@@ -3,7 +3,12 @@ import Contact from "../components/Contact/Contact"
 import HeroSection from "../components/Hero/HeroSection"
 import OrderSection from "../components/Order/OrderSection"
 import {serviceList} from "../Utility/service.json"
+import DescriptionSection from '../components/DescriptionSection'
+import {descriptionList} from "../Utility/description.json";
+
 const DigitalMarketing = () => {
+  const {backgroundImage,heading,paragraphs,imgInfo}=descriptionList[5]
+
   return (
     <Container fluid={true}>
     <Row> 
@@ -15,6 +20,9 @@ const DigitalMarketing = () => {
          paragraph={"Digital marketing solutions have replaced traditional marketing techniques, making them obsolete. Offering these outstanding digital marketing services is what sets us apart as one of Bangalore’s leading digital marketing organizations."}
          card={serviceList[5]["specificService"]}/>
         </Col>
+        <Col className="col-12 m-0 p-0  mt-4">
+      <DescriptionSection background={backgroundImage} paragraphs={paragraphs} heading={heading} imgInfo={imgInfo}/>
+      </Col>
         <Col className="col-12 mt-4 p-md-4  wow animate__animated animate__zoomIn">
           <Contact background="Contact.webp" />
         </Col>
