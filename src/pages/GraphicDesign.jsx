f9bfb328dcc5af5eecc4c1b0d4f56b030cdf1cbb
Shivam <txt2shivam@gmail.com>
2024-08-23 10:58:28 +0530

    Added DescriptionSection in Graphic Design Pages

--- src/pages/GraphicDesign.jsx
+++ src/pages/GraphicDesign.jsx
@@ -1,15 +1,21 @@
 import { Col, Container, Row } from "react-bootstrap"
 import Contact from "../components/Contact/Contact"
 import HeroSection from "../components/Hero/HeroSection"
-
+import DescriptionSection from '../components/DescriptionSection'
+import {descriptionList} from "../Utility/description.json";
  
 
 const GraphicDesign = () => {
+  const {backgroundImage,heading,paragraphs,imgInfo}=descriptionList[3]
+
   return (
     <Container fluid={true}>
     <Row> 
         <Col className="col-12 m-0 p-0">
               <HeroSection background="graphicHero.jpg" heading="Graphic" subHeading="Designing" backgroundPosition="right center"/>
+        </Col>
+        <Col className="col-12 m-0 p-0  mt-4">
+          <DescriptionSection background={backgroundImage} paragraphs={paragraphs} heading={heading} imgInfo={imgInfo}/>
         </Col>
         <Col className="col-12 mt-4 p-md-4  wow animate__animated animate__zoomIn">
           <Contact background="Contact.webp" />
