54c180627de3c0ecee72ada0e5ea9bae28a7ceae
Shivam <txt2shivam@gmail.com>
2024-08-23 12:10:09 +0530

    Added Description Section in Digital Marketing Page

--- src/pages/UiUx.jsx
+++ src/pages/UiUx.jsx
@@ -3,8 +3,13 @@ import Contact from "../components/Contact/Contact"
 import HeroSection from "../components/Hero/HeroSection"
 import OrderSection from "../components/Order/OrderSection"
 import {serviceList} from "../Utility/service.json"
+import DescriptionSection from '../components/DescriptionSection'
+import {descriptionList} from "../Utility/description.json";
+
 
 const UiUx = () => {
+  const {backgroundImage,heading,paragraphs,imgInfo}=descriptionList[4]
+
   return (
     <Container fluid={true}>
     <Row> 
@@ -14,6 +19,9 @@ const UiUx = () => {
     <Col className="m-0 p-0 col-12 mt-4 p-md-4">
       <OrderSection heading={"Different UX/UI design services we offer"} card={serviceList[2]["specificService"]}/>
      </Col>
+     <Col className="col-12 m-0 p-0  mt-4">
+      <DescriptionSection background={backgroundImage} paragraphs={paragraphs} heading={heading} imgInfo={imgInfo}/>
+      </Col>
       <Col className="col-12 mt-4 p-md-4  wow animate__animated animate__zoomIn">
           <Contact background="Contact.webp" />
         </Col>
