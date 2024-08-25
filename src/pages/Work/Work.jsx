358c71caad3fdfa62b8b3857e9bcf68aea08dca7
Shivam <txt2shivam@gmail.com>
2024-08-25 12:12:35 +0530

    Worked On Work Pages

--- src/pages/Work/Work.jsx
+++ src/pages/Work/Work.jsx
@@ -1,8 +1,37 @@
+import { Col, Container, Row, Tab, Tabs } from 'react-bootstrap';
+import { ProjectList } from "../../Utility/project.json";
+import ProjectCard from "../../components/ProjectCard";
 
-const Work = () => {
-  return (
-    <div>Work</div>
-  )
-}
 
-export default Work
+// TabContent Component
+const TabContent = ({ category }) => (
+  <Container fluid>
+    <Row className='text-center gap-3 gap-sm-5 gap-lg-5 justify-content-center  justify-content-md-center'>
+      {ProjectList.filter(item => item.category === category).map((item, ind) => (
+        <ProjectCard item={item} key={ind} />
+      ))}
+    </Row>
+  </Container>
+);
+
+const Work = () => (
+  <Container fluid>
+    <Row className='fs-4 text-black'>
+      <Col className='tab_container'>
+        <Tabs defaultActiveKey="ALL" className="mb-3 work_tab" fill>
+          <Tab eventKey="ALL" title="ALL">
+            <TabContent category="ALL" />
+          </Tab>
+          <Tab eventKey="UI/UX" title="UI/UX">
+            <TabContent category="UI/UX" />
+          </Tab>
+          <Tab eventKey="WEB DEVELOPMENT" title="WEB DEVELOPMENT">
+            <TabContent category="ALL" />
+          </Tab>
+        </Tabs>
+      </Col>
+    </Row>
+  </Container>
+);
+
+export default Work;
