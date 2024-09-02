ec235411b665817731ccb95bcd5f228672333c2f
Shivam <txt2shivam@gmail.com>
2024-09-02 17:13:23 +0530

    Added Cached

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import purgecss from 'rollup-plugin-purgecss';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), purgecss({
+    content: ['./src/**/*.html', './src/**/*.jsx'],
+  })],
 })
