c091bbfa57d1248c63c037bcbee16dccac585851
Shivam Mishra <80404991+Shivam367-glitch@users.noreply.github.com>
2024-10-20 10:50:56 +0000

    Web App Configuration

--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,15 @@ import './index.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Provider } from 'react-redux';
 import {store} from "./app/store.jsx"
+import { registerSW } from 'virtual:pwa-register';
+const updateSW = registerSW({
+  onNeedRefresh() {
+    console.log('New content is available; please refresh.');
+  },
+  onOfflineReady() {
+    console.log('App is ready for offline usage.');
+  },
+});
 ReactDOM.createRoot(document.getElementById('root')).render(
   <Provider store={store}>
     <App />
